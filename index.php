c690a0b1656e49e8dfa5779a09640e44faf22238
Zul <zm82@hw.ac.uk>
2015-02-27 23:48:24 +0400

    Added bootstrap.

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-	phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Home</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="jumbotron">
+				<h1>Hello</h1>
+			</div>
+		<div class="container">
+			<h1>Hello world</h1>
+		</div>
+	</body>
+</html>
